2963fad46220061b3cc44be0ca9363fbc9547f38
Vano <vanolaptev@mail.ru>
2022-12-26 02:17:10 +0300

    first commit

---
+++ Task56/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите
+// программу, которая будет находить строку с наименьшей 
+// суммой элементов. Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и
+// выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+var arr = FillMatrix(4, 4);
+PrintMatrix(arr);
+Console.WriteLine();
+NumberSmallSumRow(arr);
+
+void NumberSmallSumRow(int[,] array)
+{
+    int smallRow = 0;
+    int smallSumRow = 0;
+    int sumRow = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        smallRow += array[0, i];
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) sumRow += array[i, j];
+        if (sumRow < smallRow)
+        {
+            smallRow = sumRow;
+            smallSumRow = i;
+        }
+        sumRow = 0;
+    }
+    Console.Write($"{smallSumRow + 1} строка");
+}
+
+int[,] FillMatrix(int rows, int colums)
+{
+    int[,] matrix = new int[rows, colums];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+       // Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}, ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Task58/Program.cs
@@ -0,0 +1,72 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, 
+// которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+int rows = ReadInt("Введите количество строк: ");
+int columns = ReadInt("Введите количество столбцов: ");
+int[,] matrix1 = new int[rows, columns];
+int[,] matrix2 = new int[rows, columns];
+int[,] resultMatrix = new int[rows, columns];
+
+FillMatrix(matrix1);
+PrintMatrix(matrix1);
+Console.WriteLine();
+
+FillMatrix(matrix2);
+PrintMatrix(matrix2);
+Console.WriteLine();
+
+if (matrix1.GetLength(0) != matrix2.GetLength(1))
+{
+    Console.WriteLine(" Нельзя перемножить ");
+    return;
+}
+for (int i = 0; i < matrix1.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix2.GetLength(1); j++)
+    {
+        resultMatrix[i, j] = 0;
+        for (int k = 0; k < matrix1.GetLength(1); k++)
+        {
+            resultMatrix[i, j] += matrix1[i, k] * matrix2[k, j];
+        }
+    }
+}
+
+PrintMatrix(resultMatrix);
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] FillMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        // Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}, ");
+        }
+        Console.WriteLine();
+    }
+}
